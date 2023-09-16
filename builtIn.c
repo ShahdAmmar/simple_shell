05d12d21e1b36afa0f7711ab0152daec5f926567
abdallahade1 <abdoadelshabaan@gmail.com>
2023-09-16 15:38:39 -0700

    builtIn.c

--- builtIn.c
+++ builtIn.c
@@ -7,6 +7,15 @@ int check_builtIn(char *arg)
 		_puts("Exiting Shell");
 		exit(0);
 	}
+	else if (_strcmp(arg, "env") == 0)
+	{
+		char **env = environ;
+		while (*env != NULL)
+        {
+            _puts(*env);
+            env++;
+        }
+    }
 
 	return (0);
 }
