5cdd8baec8b64a5807d16db2e90f82bc57ae3cea
abdallahade1 <abdoadelshabaan@gmail.com>
2023-09-16 15:07:43 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -10,7 +10,10 @@
 
 #define MAX_LETTERS 1000
 #define INTERACTIVE (isatty(STDIN_FILENO))
+#define MAX_PATH_LENGTH 1024
 
+char *ppath(const char *arg, const char *direc);
+char *get_path(char *arg);
 int _putchar(char c);
 void _puts(char *str);
 char *_strdup(const char *str);
