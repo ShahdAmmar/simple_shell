b9fcd9a360d44ac72ddbc320ee890cfdef4c6086
abdallahade1 <abdoadelshabaan@gmail.com>
2023-09-17 12:12:10 -0700

    hsh.c

--- hsh.c
+++ hsh.c
@@ -1,19 +1,22 @@
 #include "shell.h"
-
-int main()
+/**
+ * main - the entry point function for this shell
+ * Return: return 0 on success
+ */
+int main(void)
 {
-        char **array;
+	char **array;
 	pid_t pid;
 	char *cmd;
 
-        while (1)
-        {
-                if (INTERACTIVE)
-                        prompt();
-                array = getinput();
+	while (1)
+	{
+		if (INTERACTIVE)
+			prompt();
+		array = getinput();
 		check_builtIn(array[0]);
-                cmd = get_path(array[0]);
-                if (!cmd)
+		cmd = get_path(array[0]);
+		if (!cmd)
 		{
 			_puts("command not found");
 			free(array);
@@ -25,24 +28,25 @@ int main()
 		{
 			perror("fork");
 			free(array);
-			free(cmd);			
+			free(cmd);
 			exit(EXIT_FAILURE);
 		}
 		else if (pid == 0)
 		{
 			int val = execve(cmd, array, NULL);
+
 			if (val == -1)
 			{
 				perror("excute");
 				free(array);
 				free(cmd);
 				exit(EXIT_FAILURE);
-			}			
+			}
 		}
 		else
 			wait(NULL);
-		free(array);
-		free(cmd);
-        }
-        return (0);
+			free(array);
+			free(cmd);
+	}
+	return (0);
 }
