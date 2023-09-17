cd35e2e1c774ab481976d78cb219b8ae43eb7a08
abdallahade1 <abdoadelshabaan@gmail.com>
2023-09-17 11:35:26 -0700

    builtIn.c

--- builtIn.c
+++ builtIn.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * check_builtIn - fucntion to check if the command is bultin
+ * @arg: the command to be checked
+ * Return: 0 it is not buitin
+ *		1 if is builtin
+ */
+
 int check_builtIn(char *arg)
 {
 	if (_strcmp(arg, "exit") == 0)
@@ -10,12 +17,13 @@ int check_builtIn(char *arg)
 	else if (_strcmp(arg, "env") == 0)
 	{
 		char **env = environ;
+
 		while (*env != NULL)
-        {
-            _puts(*env);
-            env++;
-        }
-    }
+		{
+			_puts(*env);
+			env++;
+		}
+	}
 
 	return (0);
 }
