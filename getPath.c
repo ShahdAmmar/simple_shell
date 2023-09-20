fc7b19f846523222fae1d85d1319fb244ab0314b
ShahdAmmar <132699172+ShahdAmmar@users.noreply.github.com>
2023-09-20 03:21:12 +0300

    Update getPath.c

--- getPath.c
+++ getPath.c
@@ -48,7 +48,7 @@ char *ppath(const char *arg, const char *direc)
  */
 char *get_path(char *arg)
 {
-	char *p = getenv("PATH");
+	char *p = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
 	char *path_cp = _strdup(p);
 	char *direc = strtok(path_cp, ":");
 	char *tot_path = NULL;
